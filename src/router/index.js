44ac50298ee1946e0f16f452608f07587ca5365e
belladev250 <lissadev17@gmail.com>
2025-08-06 22:59:30 +0200

    new chnages

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import Admin from '../pages/AdminPage.vue'
 import Login from '../pages/LoginPage.vue'
 import ReceptionDashboard from '@/components/ReceptionDashboard.vue'
 import SuperAdminDashboard from '@/components/SuperAdminDashboard.vue'
+import QrGenerator from '@/components/QrGenerator.vue'
 import CheckinPage from '../pages/CheckinPage.vue'
 const routes = [
-  { path: '/', name: 'CheckIn', component: CheckinPage },
+  { path: '/Checkin', name: 'CheckIn', component: CheckinPage },
   { path: '/admin', name: 'Admin', component: Admin },
   { path: '/login', name: 'Login', component: Login },
   { path: '/reception-dashboard', name: 'receptiondashboard', component: ReceptionDashboard },
   { path: '/super-admin-dashboard', name: 'admindashboard', component: SuperAdminDashboard },
+  { path: '/', name:"QrGenerator",component:QrGenerator}
 
 ]
 
